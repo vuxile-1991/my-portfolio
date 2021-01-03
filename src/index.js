479e40c239a9e0913f6cb1f660e4b77b589b9fc0
unknown <vuxile@gmail.com>
2021-01-03 13:09:47 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import initTilt from './js/tilt';
+import initSr from './js/sr';
+
+import './style/main.scss';
+
+import myResume from './assets/vukasin_milekic_resume.pdf';
+import './style/main.scss';
+
+function addResume(MyResume){
+  const elements = document.getElementsByClassName('.cta-btn--resume');
+  for ( let  el  of  elements ) {
+    el.href = url(MyResume);
+  }
+}
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+addResume();
+initSr();
+initTilt();
